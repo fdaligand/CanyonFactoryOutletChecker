21efb5b92cd02434abba12b5b3f97ad2d446ee3c
unknown <fdaligand@InsideGZ0755J.insidefr.com>
2015-10-21 09:29:35 +0200

    rebase Formater

---
+++ Formater/Format.py
@@ -0,0 +1,96 @@
+from Dispatcher import Event
+import yaml
+import pdb
+import smtplib
+class Format( object ):
+
+    def __init__(self,config):
+
+        self._text=""
+        self._specificText=""
+        self._filter=self._loadFilter(config)
+
+    def _loadFilter(self,config):
+
+        x = yaml.load(open(config,'r'))
+        return x.get('filter',None)
+
+
+class CanyonFormat(Format):
+
+    def __init__(self,eventDispatcher,config):
+
+        super(CanyonFormat,self).__init__(config)
+        self._eventDispatcher=eventDispatcher
+        self._eventDispatcher.addEventListener("Item", self.appendNewItem)
+
+
+    def appendNewItem(self,event):
+
+        filtredEvent = False
+        if self._filter :
+            for key,value in self._filter.items():
+                if value in event._data._data.get(key,''):
+                    filtredEvent = True
+
+        if filtredEvent:
+
+            self._specificText += """New item detected on page:
+            Serie : {serie} from year {year} in size {size}\n
+            State : {state}
+            Old Price:{oldPrice} \n
+            New Price:{newPrice} \n
+
+            Difference:{diff} \n
+
+            More info : {url}\n
+
+            _____________________________________________________________________
+            ---------------------------------------------------------------------
+            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+            """.format(serie= event._data.getSerie(),
+                      year= event._data.year,
+                      size= event._data.size,
+                      state= event._data.state,
+                      oldPrice= self.formatPrice(event._data.price),
+                      newPrice= self.getNewPrice(event._data),
+                      diff= self.formatPrice(event._data.diff),
+                      url= event._data.url)
+
+
+
+    def getNewPrice(self,model):
+
+        return self.formatPrice(model.price-model.diff)
+
+    def formatPrice(self,price):
+
+        return "%d euro"%(price/100)
+
+    def getText(self):
+
+        return self._text+self._specificText
+
+
+
+class Email( object ):
+
+    def __init__(self,config,msg):
+
+        self._from = 'florent.daligand.dev@gmail.com'
+        self._to = self.getMailList(config)
+        self._msg = msg.getText()
+
+
+    def getMailList(self,config):
+
+        conf = yaml.load(open(config,'r'))
+
+        if conf.get('email',None):
+            return conf['email']['to']
+
+    def send(self):
+
+        server = smtplib.SMTP('smtp.gmail.com')
+        server.sendmail(self._from,self._to,self._msg)
+        server.quit()
